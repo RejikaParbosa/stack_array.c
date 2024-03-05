c26e36c273368970e7af67651c8de50ed6f7acf5
Jajang-man-bungbung <91002280+RejikaParbosa@users.noreply.github.com>
2024-03-05 12:40:19 +0530

    stack_array.c

--- stack_array.c
+++ stack_array.c
@@ -1,78 +1,80 @@
+// implementation of stack using array
 #include<stdio.h>
-#include<stdio.h>
-#define MAX 5
-	int top=-1;
-	int stack_arr[MAX];
-	
-	
-	push()
+#include<stdlib.h>
+#define MAX 4
+int top=-1;
+int stack1[MAX];
+
+push()
+{
+	int pushed_item;
+	if(top==MAX-1)
 	{
-		int pushed_item;
-		if(top==(MAX-1))
-			printf("Stack overflow\n");
-		else
+		printf("\n Stack Overflow");
+	}
+	else
+	{
+		printf("\n Enter the item to be pushed in stack: ");
+		scanf("%d",&pushed_item);
+		top=top+1;
+		stack1[top]=pushed_item;
+	}
+	printf("\n");
+}
+
+pop()
+{
+	if(top==-1)
+		printf("\n Stack Underflow");
+	else
+	{
+		printf("Popped element is %d", stack1[top]);
+		top=top-1;
+	}
+	printf("\n");
+}
+
+display()
+{
+	int i;
+	if (top==-1)
+		printf("Stack is empty");
+	else
+	{
+		printf("Stack elements:\n");
+		for(i=top;i>=0;i--)
+			printf("\n %d ",stack1[i]);
+	}
+	printf("\n");
+}
+
+main()
+{
+	int choice;
+	while(1)
+	{
+		printf("\tMENU\n");
+		printf("1. Push\n");
+		printf("2. Pop\n");
+		printf("3. Display\n");
+		printf("4. Quit\n");
+		printf("Your choice: ");
+		scanf("%d",&choice);
+		switch(choice)
 		{
-			printf("Enter the item to be pushed in the stack:");
-			scanf("%d",&pushed_item);
-			top=top+1;
-			stack_arr[top]=pushed_item;
+			case 1:
+				push();
+				break;
+			case 2:
+				pop();
+				break;
+			case 3:
+				display();
+				break;
+			case 4:
+				exit(1);
+			default:
+				printf("Invalid choice");
 		}
 	}
-	
-	
-	pop()
-	{
-		if(top==-1)
-			printf("Stack underflow\n");
-		else
-		{
-			printf("Popped element is:%d\n",stack_arr[top]);
-			top=top-1;
-		}
-	}
-	
-	
-	display()
-	{
-		int i;
-		if(top==-1)
-			printf("Stack is empty\n");
-		else
-		{
-			printf("Stack elements:\n");
-			for(i=top;i>=0;i--)
-				printf("%d\n",stack_arr[i]);
-		}
-	}
-	
-	
-	main()
-	{
-		int choice;
-		while(1)
-		{
-			printf("1. Push\n");
-			printf("2. Pop\n");
-			printf("3. Display\n");
-			printf("4. Quit\n");
-			printf("Enter your choice:");
-			scanf("%d",&choice);
-			switch(choice)
-			{
-				case 1:
-					push();
-					break;
-				case 2:
-					pop();
-					break;
-				case 3:
-					display();
-					break;
-				case 4:
-					exit(1);
-				default:
-					printf("Wrong choice\n");
-			}
-		}
-	}
-					
+}
